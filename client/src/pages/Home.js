da0502a236934d0537175cc01b804e59bc688463
StevenDong2512 <stevendong1997@outlook.com>
2023-07-18 16:00:08 +1000

    Pages styled v1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,28 @@
 import React from "react";
-import "./Page.css";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   return (
     <div className="home-content">
       <div className="container">
-        <h1>Welcome to the eCommerce Revolve.</h1>
-        <p>
-          This is where you can find the user reviews for the shopping
-          experience throught major eCommerce marketplaces in Australia.
-        </p>
+        <h1>
+          eCommerce Revolve, a place where you can share your shopping
+          experiences.
+        </h1>
+        <div className="marketplace-list">
+          <div className="marketplace-item">
+            <Link to="/marketplace1">
+              {/* <img src={placeholderImage1} alt="Marketplace 1" /> */}
+            </Link>
+          </div>
+          <div className="marketplace-item">
+            <Link to="/marketplace2">
+              {/* <img src={placeholderImage2} alt="Marketplace 2" /> */}
+            </Link>
+          </div>
+          {/* Add more marketplace placeholders as needed */}
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,11 @@
 import React from "react";
 import LoginForm from "../components/auth/LoginForm";
-import "./Page.css";
+import "./Login.css";
 
 function Login() {
   return (
     <div className="login-content">
-      <h1>Login</h1>
+      <h1 className="login-heading">Login</h1>
       <LoginForm />
     </div>
   );
