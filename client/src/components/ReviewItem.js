f53fd860775e161541abcff4872dc0668646001d
Shiby-Mathew <shiby.ajay@gmail.com>
2023-07-23 13:12:13 +1000

    changed

--- client/src/components/ReviewItem.js
+++ client/src/components/ReviewItem.js
@@ -2,11 +2,13 @@ import React from "react";
 // import "./ReviewItem.css";
 
 function ReviewItem({ review }) {
+  console.log(review);
   return (
     <div className="review-item">
       <h3 className="title">{review.title}</h3>
-      <p className="description">{review.description}</p>
-      <p className="author">By: {review.created_date}</p>
+      <p className="description">{review.review}</p>
+      <p className="author"> {review.location}</p>
+      {/* <p className="author">By: {review.author}</p> */}
     </div>
   );
 }
